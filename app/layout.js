69483a7ad111441ad7f5d9e5cd6c58529e8bb9c4
Harsha Shetty <harshashetty@Harshas-MacBook-Air.local>
2025-08-14 08:54:18 +0530

    first commit

---
+++ app/layout.js
@@ -0,0 +1,23 @@
+import {Inter} from "next/font/google";
+import "./globals.css";
+import Header from "../components/Header"
+import { ClerkProvider }  from '@clerk/nextjs'
+
+const inter = Inter({subsets:["latin"]});
+export const metadata = {
+  title:"DriveIQ",
+  description:"Find your dream car",
+}
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider>
+    <html lang="en">
+      <body className={`${inter.className}`}>
+        <Header/>
+        <main className="min-h-screen">{children}</main>
+
+      </body>
+    </html>
+    </ClerkProvider>
+  );
+}
