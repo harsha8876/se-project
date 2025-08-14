b4a50307c62dae0ff90d16e0e670f1440c46009e
Harsha Shetty <harshashetty@Harshas-MacBook-Air.local>
2025-08-14 09:43:01 +0530

    chnages in middleware

--- middleware.js
+++ middleware.js
@@ -8,7 +8,7 @@ const isProtectedRoute = createRouteMatcher([
 
 export default clerkMiddleware(async (auth,req)=>{
   const {userId} = await auth();
-  if(!userdId && isProtectedRoute(req)){
+  if(!userId && isProtectedRoute(req)){
     const {redirectToSignIn} = await auth();
     return redirectToSignIn();
   } 
