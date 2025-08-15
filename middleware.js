9d51b6d0f1a7399bbd71ba9e0d04ab913ad1a703
K3V1N0123 <131840363+K3V1N0123@users.noreply.github.com>
2025-08-16 00:23:54 +0530

    Fix Clerk middleware auth usage

--- middleware.js
+++ middleware.js
@@ -7,9 +7,8 @@ const isProtectedRoute = createRouteMatcher([
 ])
 
 export default clerkMiddleware(async (auth,req)=>{
-  const {userId} = await auth();
+  const {userId, redirectToSignIn} = auth;
   if(!userId && isProtectedRoute(req)){
-    const {redirectToSignIn} = await auth();
     return redirectToSignIn();
   } 
 });
