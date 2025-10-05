e5ae882a96ba84f8ed51d67368536c98b1d9541e
Harsha Shetty <harshashetty@Harshas-MacBook-Air.local>
2025-10-06 00:34:28 +0530

    add car

--- app/(admin)/admin/cars/create/page.jsx
+++ app/(admin)/admin/cars/create/page.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import AddCarform from '../components/add-car-form';
+import { AddCarForm } from '../components/add-car-form';
 
 export const metadata = {
     title: "Add new car",
@@ -10,7 +10,7 @@ const AddCarpage = () => {
   return (
     <div className='p-6'>
         <h1 className="text-2xl font-bold mb-6">Add New Car</h1>
-        <AddCarform/>
+        <AddCarForm/>
     </div>
   )
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsHmrCache: false,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "augjllolpzqqkqwxjcip.supabase.co",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
